e43cc1891c9c3d1d0f5ef3c7fae8440cc397a1c5
Leonardo Mazzon <leonardomazzon5@gmail.com>
2023-05-27 20:23:14 +0200

    Added class Semaphore with tryLock

---
+++ src/com/leonardomazzon/artistadistrada/Semaphore.java
@@ -0,0 +1,42 @@
+package com.leonardomazzon.artistadistrada;
+
+public class Semaphore {
+	protected int s;
+	
+	public Semaphore(int n) {
+		s = n;
+	}
+	
+	/**
+	 * Try to lock the semaphore, if it fails it doesn't block the thread
+	 * @return the result of the lock operation, true if the semaphore can be locked, false otherwise
+	 */
+	synchronized public boolean tryLock() {
+		boolean res = false;
+		
+		//if the semaphore can be locked
+		if(s > 0) {
+			lock();
+			res = true;
+		}
+		
+		return res;
+	}
+	
+	synchronized public void lock() {
+		s--;
+		
+		if(s < 0) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	synchronized public void unlock() {
+		s++;
+		this.notify();
+	}
+}
