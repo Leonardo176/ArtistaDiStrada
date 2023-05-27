6ba281692c73ad9413560dba575f25a9e6d007c2
Leonardo Mazzon <leonardomazzon5@gmail.com>
2023-05-27 20:55:43 +0200

    Fixed typo

--- src/com/leonardomazzon/artistadistrada/IntRand.java
+++ src/com/leonardomazzon/artistadistrada/IntRand.java
@@ -1,7 +1,7 @@
 package com.leonardomazzon.artistadistrada;
 
 /**
- * This class allows you to generate randomy an Integer
+ * This class allows you to generate randomly an integer
  * @author Leonardo Mazzon 4CIA
  * @version 2.0
  */
@@ -11,7 +11,7 @@ public class IntRand {
 	 * Generate randomly an integer
 	 * @param min Minimum integer generated
 	 * @param max Maximum integer generated
-	 * @return Random number between min and max
+	 * @return Random integer between min and max
 	 */
 	public static int getRandInt(int min, int max) {
 		return (int) (min+(max-min+1)*Math.random());
